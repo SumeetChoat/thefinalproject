e59aa38a00007e177b11ab38c75be069fa6aca20
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-08-07 14:51:11 +0100

    setup dtb

--- server/database/setup.js
+++ server/database/setup.js
@@ -4,4 +4,8 @@ require('dotenv').config()
 const db = require('./connect')
 const sql = fs.readFileSync('./database/setup.sql').toString()
 
-
+db.query(sql)
+.then(data => {
+    console.log('Set up complete.')
+})
+.catch(err => console.log(err))
--- server/database/setup.sql
+++ server/database/setup.sql
@@ -3,11 +3,12 @@ DROP TABLE IF EXISTS students, teachers, assignments;
 CREATE TABLE students (
     student_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     username VARCHAR NOT NULL,
-    password VARCHAR NOT NULL
+    password VARCHAR NOT NULL,
+    token VARCHAR(36)
 );
 
 CREATE TABLE teachers (
-    teacher_id INTEGER PRIMARY KEY,
+    teacher_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     username VARCHAR NOT NULL,
     password VARCHAR NOT NULL,
     email VARCHAR NOT NULL
@@ -19,11 +20,11 @@ CREATE TABLE student_teacher (
 );
 
 CREATE TABLE assignments (
-    assignment_id INT GENERATED ALWAYS AS PRIMARY KEY,
+    assignment_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     student_id INT REFERENCES students(student_id),
     teacher_id INT REFERENCES teachers(teacher_id),
-    range ARRAY,
-    pattern ARRAY,
+    range INT ARRAY,
+    pattern INT ARRAY,
     completed BOOLEAN,
     score INT,
     hand VARCHAR
