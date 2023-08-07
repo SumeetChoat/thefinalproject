8d1685df52f889e1e14b339d42e176a4fefa6680
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-08-07 16:33:06 +0100

    student routes

--- server/controllers/studentsController.js
+++ server/controllers/studentsController.js
@@ -19,10 +19,42 @@ class StudentsController {
         }
     }
 
+    static async login(req,res) {
+        try {
+            const data = req.body
+            const student = await Students.getByUsername(data.username)
+            const auth = await bcrypt.compare(
+                data.password,
+                student["password"]
+            )
+            if (!auth) {
+                throw new Error("Wrong username or password")
+            } else {
+                res.status(200).json({authenticated: true, token: student["token"], username: student["username"]})
+            }
+        } catch (err) {
+            console.log(err)
+            res.status(403).json({Error: err.message})
+        }
+    }
+
     static async getStudents(req,res) {
         try {
             const students = await Students.getStudents()
             res.status(200).json(students)
+        } catch (err) {
+            console.log(err)
+            res.status(404).json({Error: err.message})
+        }
+    }
+
+    static async getOneByID(req,res) {
+        try {
+            // token
+            const id = 1
+            const student = await Students.getOneByID(id)
+            delete student.password
+            res.status(200).send(student)
         } catch (err) {
             console.log(err)
             res.status(404).json({Error: err.message})
@@ -38,6 +70,30 @@ class StudentsController {
         } catch (err) {
             console.log(err)
             res.status(404).json({Error: err.message})
+        }
+    }
+
+    static async logout(req,res) {
+        try {
+            // token
+            const id = req.body.student_id
+            const student = await Students.getOneByID(id)
+            res.status(202).json({ message: student})
+        } catch (err) {
+            console.log(err)
+            res.status(403).json({Error: err.message})
+        }
+    }
+
+    static async deleteUser(req,res) {
+        try {
+            const id = req.body.student_id
+            const student = await Students.getOneByID(id)
+            const resp = await student.deleteStudent(id)
+            res.status(204).json(resp)
+        } catch (err) {
+            console.log(err)
+            res.status(403).json({Error: err.message})
         }
     }
 }
--- server/models/Students.js
+++ server/models/Students.js
@@ -13,6 +13,29 @@ class Students {
         this.points = points
     }
 
+    static async getStudents() {
+        const resp = await db.query("SELECT student_id,username,email,token,score")
+        if (resp.rows.length == 0) {
+            throw new Error('There are no students')
+        } else {
+            return resp.rows.map((s) => new Students(s))
+        }
+    }
+
+    static async getByUsername(username) {
+        const resp = await db.query("SELECT * FROM students WHERE username = $1",[username])
+        if (resp.rows.length == 0){
+            throw new Error("Student with this username does not exist")
+        } else {
+            return new Students(resp.rows[0])
+        }
+    }
+
+    static async getOneByID(id) {
+        const resp = await db.query("SELECT * FROM students WHERE student_id = $1",[id])
+        return new Students(resp.rows[0])
+    }
+
     static async createStudent(data) {
         const { username, password, email, points } = data
         const token = uuidv4()
@@ -22,24 +45,6 @@ class Students {
         return newStudent
     }
 
-    static async getOneByID(id) {
-        const resp = await db.query("SELECT * FROM students WHERE student_id = $1",[id])
-        return new Students(resp.rows[0])
-    }
-
-    static async getByUsername(username) {
-        const resp = await db.query("SELECT * FROM students WHERE username = $1",[username])
-    }
-
-    static async getStudents() {
-        const resp = await db.query("SELECT user_id,username,email,token,score")
-        if (resp.rows.length == 0) {
-            throw new Error('There are no users')
-        } else {
-            return resp.rows.map((s) => new Students(s))
-        }
-    }
-
     static async getAssignments(student_id) {
         const resp = await db.query("SELECT * FROM assignments WHERE student_id = $1",[student_id])
         if (resp.rows.length == 0) {
@@ -47,6 +52,12 @@ class Students {
         } else {
             return resp.rows.map((a) => a)
         }
+    }
+
+    async deleteStudent() {
+        const resp = await db.query("DELETE FROM student_teacher WHERE student_id = $1",[this.student_id])
+        const resp2 = await db.query("DELETE FROM students WHERE student_id = $1 RETURNING *",[this.student_id])
+        return new Students(resp2.rows[0])
     }
 }
 
