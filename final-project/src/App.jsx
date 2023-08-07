48cf5e94e2015c4f76457729afddd0ce9b2cb5e8
anthonychan1211 <anthonychan1211@gmail.com>
2023-08-07 16:33:37 +0100

    updating first note, but the index keeps going up

--- final-project/src/App.jsx
+++ final-project/src/App.jsx
@@ -1,12 +1,97 @@
 import "./App.css";
 import StaveComponent from "./components/StaveComponent";
-
+import { useEffect, useState } from "react";
+import { autoCorrelate } from "./lib/pitchDetaction";
 function App() {
-  const note = [72, 74];
+  const [currentIndex, setCurrentIndex] = useState(-1);
+  const [challenge, setChallenge] = useState([
+    { note: 60, isCorrect: false },
+    { note: 62, isCorrect: false },
+    { note: 64, isCorrect: false },
+    { note: 65, isCorrect: false },
+  ]);
 
+  let audioContext = null;
+  let analyser = null;
+  let mediaStreamSource = null;
+
+  window.onload = function () {
+    audioContext = new AudioContext();
+  };
+
+  function startPitchDetect() {
+    // grab an audio context
+    audioContext = new AudioContext();
+
+    // Attempt to get audio input
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: {
+          mandatory: {
+            googEchoCancellation: "false",
+            googAutoGainControl: "false",
+            googNoiseSuppression: "false",
+            googHighpassFilter: "false",
+          },
+          optional: [],
+        },
+      })
+      .then((stream) => {
+        // Create an AudioNode from the stream.
+        mediaStreamSource = audioContext.createMediaStreamSource(stream);
+
+        // Connect it to the destination.
+        analyser = audioContext.createAnalyser();
+        analyser.fftSize = 2048;
+        mediaStreamSource.connect(analyser);
+        updatePitch();
+      })
+      .catch((err) => {
+        // always check for errors at the end.
+        console.error(`${err.name}: ${err.message}`);
+        alert("Stream generation failed.");
+      });
+  }
+
+  const buflen = 2048;
+  var buf = new Float32Array(buflen);
+
+  function noteFromPitch(frequency) {
+    var noteNum = 12 * (Math.log(frequency / 440) / Math.log(2));
+    return Math.round(noteNum) + 69;
+  }
+
+  function updatePitch() {
+    analyser.getFloatTimeDomainData(buf);
+    var ac = autoCorrelate(buf, audioContext.sampleRate);
+
+    if (ac == -1) {
+      // Not hearing anything
+    } else {
+      // I need this part
+      let pitch = ac;
+      var note = noteFromPitch(pitch);
+
+      if (note == challenge[currentIndex].note) {
+        const newState = [...challenge];
+        newState[currentIndex].isCorrect = true;
+
+        setChallenge(newState);
+      }
+    }
+
+    if (!window.requestAnimationFrame)
+      window.requestAnimationFrame = window.webkitRequestAnimationFrame;
+    window.requestAnimationFrame(updatePitch);
+  }
+  useEffect(() => {
+    setCurrentIndex((prev) => prev + 1);
+  }, [challenge]);
+  console.log(currentIndex);
   return (
     <>
-      <StaveComponent note={note} />
+      <button onClick={startPitchDetect}>Start</button>
+      <StaveComponent challenge={challenge} />
     </>
   );
 }
--- final-project/src/components/StaveComponent/index.jsx
+++ final-project/src/components/StaveComponent/index.jsx
@@ -9,7 +9,7 @@ import {
   Voice,
   Beam,
 } from "vexflow";
-function StaveComponent({ note }) {
+function StaveComponent({ challenge }) {
   const noteStrings = [
     "C",
     "C#",
@@ -27,14 +27,6 @@ function StaveComponent({ note }) {
   const score = useRef(null);
   useEffect(() => {
     if (document.querySelector("svg")) document.querySelector("svg").remove();
-    const noteArr = note.map((n) => {
-      const octave = n < 24 ? 0 : Math.floor((n - 24) / 12) + 1;
-      const noteName = n % 12;
-      return new StaveNote({
-        keys: [`${noteStrings[noteName][0]}/${octave}`],
-        duration: "4",
-      });
-    });
 
     const renderer = new Renderer(score.current, Renderer.Backends.SVG);
 
@@ -51,6 +43,18 @@ function StaveComponent({ note }) {
 
     stave1.setContext(context);
     stave2.setContext(context);
+    const noteArr = challenge.map((n) => {
+      const octave = n.note < 24 ? 0 : Math.floor((n.note - 24) / 12) + 1;
+      const noteName = n.note % 12;
+      return new StaveNote({
+        keys: [`${noteStrings[noteName][0]}/${octave}`],
+        duration: "4",
+        auto_stem: true,
+      }).setStyle({
+        fillStyle: n.isCorrect ? "green" : "",
+        strokeStyle: n.isCorrect ? "green" : "",
+      });
+    });
     const connector = new StaveConnector(stave1, stave2);
     const line = new StaveConnector(stave1, stave2);
     connector.setType("brace");
@@ -64,15 +68,17 @@ function StaveComponent({ note }) {
     stave2.draw();
     connector.draw();
     line.draw();
-    const voice = new Voice({ num_beats: note.length, beat_value: 4 });
+    const voice = new Voice({
+      num_beats: 4,
+      beat_value: 4,
+    });
     voice.addTickables(noteArr);
     const beams = Beam.generateBeams(noteArr);
-    new Formatter().joinVoices([voice]).format([voice]);
+    Formatter.FormatAndDraw(context, stave1, noteArr);
     beams.forEach((b) => {
       b.setContext(context).draw();
     });
-    voice.draw(context, stave1);
-  }, [note]);
+  }, [challenge]);
   return (
     <div ref={score}>
       <div className="svg-container" id="output"></div>
