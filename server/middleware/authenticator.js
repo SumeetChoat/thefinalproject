b496eff297ce56cf56e972532210955ae14e17ba
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-08-10 15:09:27 +0100

    redid authenticator

--- server/middleware/authenticator.js
+++ server/middleware/authenticator.js
@@ -1,32 +1,18 @@
-const Teacher = require('../models/Teachers')
-const Students = require('../models/Students')
+const Token = require('../models/Token')
 
 async function authenticator(req, res, next) {
     try {
         const userToken = req.headers["token"]
-        const username = req.headers["username"]
-        const userType = req.headers["role"]
 
         if (userToken == "null") {
-            throw new Error("User not authenticated");
-        } else if (userType === 'student') {
-            const user = await Students.getOneByUsername(username);
-            if (user.token === userToken) {
-                next()
-            } else {
-                throw new Error("User not authenticated");
-            }
+            throw new Error("User not authenticated.");
         } else {
-            const user = await Teacher.getOneByUsername(username)
-            if (user.token === userToken) {
-                next()
-            } else {
-                throw new Error("User not authenticated");
-            }
+            req.tokenObj = await Token.getOneByToken(userToken)
+            next()
         }
     } catch(err) {
         console.log(err)
-        res.sendStatus(403)
+        res.status(403).json({"error": err.message})
     }
 }
 
