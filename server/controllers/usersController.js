25198f779103f0dc1a216300a65f022175071229
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-08-10 16:21:32 +0100

    login route

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,9 +1,7 @@
-const Users = require('../models/Users')
-const Assignments = require('../models/Assignments')
-const StudentTeacher = require('../models/StudentTeacher')
-const Students = require('../models/Students')
 require('dotenv').config()
+const Users = require('../models/Users')
 const bcrypt = require('bcrypt')
+const Token = require('../models/Token')
 
 class UserController {
     static async register(req,res){
@@ -16,6 +14,25 @@ class UserController {
 
             const user = await Users.register(data)
             res.status(201).send(user)
+        } catch (err) {
+            res.status(403).json({"error": err.message})
+        }
+    }
+
+    static async login(req,res){
+        try {
+            const data = req.body
+            const user = await Users.getByUsername(data.username)
+            const authenticated = bcrypt.compare(data["password"], user["password"])
+            if (!authenticated){
+                throw new Error('Wrong username or password')
+            } else {
+                const token = await Token.create(user["username"])
+                res.status(201).json({
+                    "authenticated": true,
+                    "token": token.token
+                })
+            }
         } catch (err) {
             res.status(403).json({"error": err.message})
         }
--- server/routes/users.js
+++ server/routes/users.js
@@ -5,6 +5,7 @@ const usersController = require('../controllers/usersController')
 const userRouter = Router();
 
 userRouter.post('/register',usersController.register)
+userRouter.post('/login', usersController.login)
 
 userRouter.use(authenticator)
 
