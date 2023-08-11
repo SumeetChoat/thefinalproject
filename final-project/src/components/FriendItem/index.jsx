f9db407154c6efe35bf2f857d5b8a76c8bd466d1
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-08-11 12:45:48 +0100

    friends list in profile page

---
+++ final-project/src/components/FriendItem/index.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react"
+import { useFriends } from "../../contexts"
+import '../../pages/profilePage/styles.css'
+
+function FriendItem({friend}){
+    console.log(friend)
+    const {friends,setFriends} = useFriends()
+
+    function deleteFriend(friend){
+         if (confirm(`Are you sure you want to remove ${friend.username} as a friend?`)==true) {
+            setFriends(
+                friends.filter(f => f.username != friend.username)
+            )
+         }
+    }
+
+    function messageFriend(friend){
+        console.log('messaging ',friend.username)
+    }
+
+    return (
+        <li className="friends-item">
+            <span className="friend-username">
+                {friend.username}
+            </span>
+            <button className="delete-friend-btn" onClick={()=>deleteFriend(friend)}>
+                Delete
+            </button>
+            <button className="message-friend-btn" onClick={()=>messageFriend(friend)}>
+                Message
+            </button>
+        </li>
+    )
+}
+
+export default FriendItem
--- final-project/src/pages/profilePage/index.jsx
+++ final-project/src/pages/profilePage/index.jsx
@@ -1,9 +1,37 @@
-import React from React
 import { useState, useEffect } from "react"
+import { useAuth, useRole} from "../../contexts"
+import Friends from "../../components/Friends"
+import './styles.css'
 
 function ProfilePage () {
-    return(
-        <>
-        </>
-    )
+    const token = useAuth().token || localStorage.getItem('token')
+    const {role} = useRole()
+
+    if (role === 'student' || role==='teacher'){
+        return(
+            <>
+            <div className="profile-container">
+            <div className="assignments-container">
+                <p></p>
+            </div>
+            <div className="friends-container">
+                {<Friends/>} 
+            </div> 
+            {role=='student'?
+                <p></p>
+            :
+                <p></p>
+            }
+            </div>
+            </>
+        )
+    } else {
+        return(
+            <div className="profile-container">
+            <p>Log in to see your profile</p>    
+            </div>
+        )
+    }
 }
+
+export default ProfilePage
