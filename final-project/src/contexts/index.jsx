f9996a2b541d887ade1b7b5fb2e79e3b3903b02c
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-08-11 10:44:51 +0100

    creating contexts

--- final-project/src/contexts/index.jsx
+++ final-project/src/contexts/index.jsx
@@ -5,13 +5,97 @@ import { useState, useContext, createContext } from "react";
 export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState();
-  const [currentAssignment, setCurrentAssignment] = useState();
   return (
     <AuthContext.Provider
-      value={{ token, setToken, currentAssignment, setCurrentAssignment }}
+      value={{ token, setToken }}
     >
       {children}
     </AuthContext.Provider>
   );
 };
 export const useAuth = () => useContext(AuthContext);
+
+//role - student or teacher
+export const RoleContext = createContext()
+export const RoleProvider = ({ children }) => {
+  const [role, setRole] = useState()
+  return (
+    <RoleContext.Provider
+      value={{ role, setRole }}
+    >
+      {children}
+    </RoleContext.Provider>
+  )
+}
+export const useRole = () => useContext(RoleContext)
+
+//assignments
+export const AssignmentsContext = createContext();
+export const AssignmentsProvider = ({ children }) => {
+  const [currentAssignment, setCurrentAssignment] = useState();
+  return (
+    <AssignmentsContext.Provider
+      value={{ currentAssignment, setCurrentAssignment }}
+    >
+      {children}
+    </AssignmentsContext.Provider>
+  );
+};
+export const useAssignments = () => useContext(AssignmentsContext);
+
+//friends
+export const FriendsContext = createContext()
+export const FriendsProvider = ({ children }) => {
+  const [friends, setFriends] = useState()
+  return (
+    <FriendsContext.Provider
+      value={{ friends, setFriends }}
+    >
+      {children}
+    </FriendsContext.Provider>
+  )
+}
+export const useFriends = () => useContext(FriendsContext)
+
+//teacher's students
+export const StudentsContext = createContext()
+export const StudentProvider = ({ children }) => {
+  const [students, setStudents] = useState()
+  return (
+    <StudentsContext.Provider
+      value={{ students, setStudents }}
+    >
+      {children}
+    </StudentsContext.Provider>
+  )
+}
+export const useStudents = () => useContext(StudentsContext)
+
+//pending requests - sent and received
+export const RequestsContext = createContext()
+export const RequestsProvider = ({ children }) => {
+  const [sentRequests, setSentRequests] = useState()
+  const [receivedRequests, setReceivedRequests] = useState()
+  return (
+    <RequestsContext.Provider
+      value={{ sentRequests, setSentRequests, receivedRequests, setReceivedRequests }}
+    >
+      {children}
+    </RequestsContext.Provider>
+  )
+}
+export const useSentRequests = () => useContext(RequestsContext)
+
+//message history
+export const MessagesContext = createContext()
+export const MessagesProvider = ({ children }) => {
+  const [messages, setMessages] = useState()
+  return (
+    <MessagesContext.Provider
+      value={{ messages, setMessages }}
+    >
+      {children}
+    </MessagesContext.Provider>
+  )
+}
+export const useMessages = () => useContext(MessagesContext)
