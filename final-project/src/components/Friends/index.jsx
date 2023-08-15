3b47666014c19f7cdd36fe8ed26aa7baf0c88c82
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-08-15 10:11:04 +0100

    styling, removing console logs£

--- final-project/src/components/Friends/index.jsx
+++ final-project/src/components/Friends/index.jsx
@@ -4,20 +4,17 @@ import {
 } from "../../contexts";
 import FriendItem from "./FriendItem";
 import '../../pages/profilePage/styles.css'
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { socket } from "../../socket";
 
 function Friends ({trash,message,add,setShowMessages}){
     const token = useAuth().token || localStorage.getItem('token')
     const {user} = useAuth()
     const {friends,setFriends} = useFriends()
-    console.log(friends)
-    console.log(user)
 
     const [friendUsernames, setFriendUsernames] = useState([])
 
     function getUsernames() {
-        console.log('get usernames')
         setFriendUsernames(friends.map((f) => {
             if(f.user1 !== user.username){
                 return {"username": f.user1}
@@ -25,16 +22,12 @@ function Friends ({trash,message,add,setShowMessages}){
                 return {"username": f.user2}
             }
         }))  
-        console.log(friendUsernames)
     }
 
     useEffect(() => {
-        console.log('friends: ',friends)
         if (friends) {  
-            console.log(friends)
             getUsernames()
         }
-        console.log(friendUsernames)
     },[friends])
 
     const [textFilter,setTextFilter] = useState('')
@@ -44,8 +37,6 @@ function Friends ({trash,message,add,setShowMessages}){
     }
 
     async function addFriend(searchText) {
-        console.log(searchText, searchText.length)
-        console.log('add')
         const options = {
             method: "GET",
             headers: {
@@ -58,8 +49,6 @@ function Friends ({trash,message,add,setShowMessages}){
         const data = await resp.json()
         if (resp.ok && (!friendUsernames.find((f) => f.username == searchText)) && searchText!==user.username && searchText.length>0) {
             console.log(data)
-            // socket
-
             socket.emit("friend_req", {"sender":user.username, "recipient":searchText});
 
             alert(`You have sent a friend request to user ${searchText}`)
