e8d9dd74da1f83051e1762b981a482ba49564c56
santhi26 <shanthipriya.addanki@gmail.com>
2023-08-15 09:19:30 +0100

    changes

--- final-project/src/pages/challengePage/challengePage.test.jsx
+++ final-project/src/pages/challengePage/challengePage.test.jsx
@@ -10,39 +10,58 @@ import App from "../../App.jsx";
 import matchers from "@testing-library/jest-dom/matchers";
 expect.extend(matchers);
 
-describe('Challenge Page when logged in', ()=>{
-  beforeEach(async () => {
-      render(
-          <Router>
-          <MockAuthProvider>
-            <RoleProvider>
-              <AssignmentsProvider>
-                <FriendsProvider>
-                  <StudentsProvider>
-                  <RequestsProvider>
-                  <MessagesProvider>
-        <Routes>
-      <Route path="/challenge" element={<challengePage />} />
-    </Routes>
-      </MessagesProvider>
-      </RequestsProvider>
-      </StudentsProvider>
-      </FriendsProvider>
-      </AssignmentsProvider>
-      </RoleProvider>
-      </MockAuthProvider>
-      </Router >
-        );
-  })
 
-  afterEach(() => {
-      cleanup()
-  })
+// Mocking modules used by the component
+jest.mock('../../contexts', () => ({
+  useAssignments: () => ({
+    currentAssignment: {
+      // mock assignment data
+    },
+    setCurrentAssignment: jest.fn(),
+  }),
+  useAuth: () => ({
+    user: {
+      // mock user data
+    },
+  }),
+}));
 
-  it("should load a timer", () => {
-      const timer = screen.getByTestId("timer")
-      expect(timer).toBeInTheDocument()
-  })
+// Mocking getUserMedia function for pitch detection
+window.navigator.mediaDevices = {
+  getUserMedia: jest.fn(() =>
+    Promise.resolve({
+      getTracks: () => [],
+    })
+  ),
+};
 
+test('renders the component', () => {
+  render(<challengePage />);
+  // Add assertions to test the rendering and presence of specific elements
+});
 
-})
+test('starts pitch detection when the "Start" button is clicked', async () => {
+  render(<challengePage />);
+
+  const startButton = screen.getByText('Start');
+  fireEvent.click(startButton);
+
+  // Add assertions to test if pitch detection logic is working as expected
+  // You might need to use async/await and waitFor here
+});
+
+test('generates a new challenge when the "Next" button is clicked', () => {
+  render(<challengePage />);
+  const nextButton = screen.getByText('Next');
+  fireEvent.click(nextButton);
+
+  // Add assertions to test if a new challenge is generated as expected
+});
+
+test('quits assignment and resets when "Quit Assignment" button is clicked', () => {
+  render(<challengePage />);
+  const quitButton = screen.getByText('Quit Assignment');
+  fireEvent.click(quitButton);
+
+  // Add assertions to test if assignment is reset and round is null
+});
