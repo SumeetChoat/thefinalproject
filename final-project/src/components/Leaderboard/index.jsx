63a62fc51cd1571f4abf8448551d369aa43ef05b
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-08-16 11:06:08 +0100

    leaderboard

--- final-project/src/components/Leaderboard/index.jsx
+++ final-project/src/components/Leaderboard/index.jsx
@@ -3,6 +3,7 @@ import { useAuth } from "../../contexts"
 
 function LeaderBoard(){
     const token = localStorage.getItem('token') || useAuth().token
+    const {user} = useAuth()
     const [users,setUsers] = useState()
 
     async function getUsers() {
@@ -30,15 +31,16 @@ function LeaderBoard(){
     return (
         <>
         <ul className="leaderboard-list">
-            {users && users.length > 0 ?
-            users.map((u, i) => {
-                return <li key={i} className="leaderboard-item">
+            {user && users && users.length > 0 ?
+            users.slice(0,10).map((u, i) => {
+                const className = u.username == user.username ? 'lb-item lb-self' : 'lb-item lb-other'
+                return <li key={i} className={className}>
                     <span className="lb-username">{u.username}</span>
                     <span className="lb-points">{u.points}</span>
                 </li>
             })  
             :
-            ""
+            <p>No users yet.</p>
             }
         </ul>
         </>
